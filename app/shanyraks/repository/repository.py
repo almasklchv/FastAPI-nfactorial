d05cc170208cf41c95947e648adc97322837546d
Almas Kilichov <itsmywaytoamerica@gmail.com>
2023-06-14 01:47:12 +0600

    feat: finish task#3 and task#4

---
+++ app/shanyraks/repository/repository.py
@@ -0,0 +1,30 @@
+from bson.objectid import ObjectId
+from pymongo.database import Database
+
+
+class ShanyrakRepository:
+    def __init__(self, database: Database):
+        self.database = database
+
+    def create_shanyrak(self, input: dict):
+        payload = {
+            "type": input['type'],
+            "price": input['price'],
+            "address": input['address'],
+            "area": input['area'],
+            "rooms_count": input['rooms_count'],
+            "description": input['description'],
+            "user_id": input['user_id']
+        }
+
+        self.database['shanyraks'].insert_one(payload)
+
+    def get_shanyrak(self, user_id: str, shanyrak_id: str):
+        shanyraq = self.database["shanyraks"].find_one(
+            {
+                "_id": ObjectId(shanyrak_id),
+            }
+        )
+
+        if (shanyraq['user_id'] == user_id):
+            return shanyraq
